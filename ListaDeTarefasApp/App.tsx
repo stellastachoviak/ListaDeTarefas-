7805b959ee93813008a525518c5c33c07585e1b6
stellastachoviak <stella_ferreira@estudante.sesisenai.org.br>
2025-06-24 16:46:41 -0300

    blablabla

--- ListaDeTarefasApp/App.tsx
+++ ListaDeTarefasApp/App.tsx
@@ -1,7 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen";
-import DetailsScreen from "./screens/DetailsScreen";
+import DetailsScreen from "./screens/DetailsScreens";
+
 
 
 const Stack = createStackNavigator();
